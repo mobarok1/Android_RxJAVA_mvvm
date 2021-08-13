2fa8c21b64e497606f93ccf686b57cf078cc3411
mobarok <mobarok.development@gmail.com>
2021-08-13 18:54:28 +0600

    Initial commit

---
+++ app/src/main/java/com/mobarok/rxjavaandmvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mobarok.rxjavaandmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
